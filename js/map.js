ed6e1db6a93bb893bd160c6c76370647bbd42552
ganga <m.gangabhavani@gmail.com>
2016-05-01 06:21:36 -0400

    marker animation

--- js/map.js
+++ js/map.js
@@ -1,32 +1,25 @@
 
 var map;
-var model = {
-
-	restaurants: [
-		{
+var model = [{
 		 name: "Paci Restaurant",
-		 loc: {lat: 41.121115, lng: -73.285464},
+		 loc: {lat: 41.143405, lng: -73.285961},
 		 address: ["Governor John Davis Lodge Turnpike", "Southport", "CT"]
 		},
 		{
 		 name: "Chips Restaurant",
-		 loc: {lat: 41.181515, lng: -73.240850},
+		 loc: {lat: 41.178543, lng: -73.240930},
 		 address: ["525 Tunxis Hill Cut Off", "Fairfield", "CT"]
-		}],
-
-	hotels: [
+		},
 		{
 		 name: "Holiday Inn",
-		 loc: {lat: 41.182853, lng: -73.188513},
+		 loc: {lat: 41.182853, lng: -73.182487},
 		 address: ["1070 Main St", "Bridgeport", "CT"]
 		},
 		{
 		 name: "Rodeway Inn",
-		 loc: {lat: 41.182853, lng: -73.188513},
+		 loc: {lat: 41.147831, lng: -73.129329},
 		 address: ["10 Washington Pkwy", "Stratford", "CT"]
-		}],
-
-	bars_and_pubs: [
+		},
 		{
 		 name: "Little Barn",
 		 loc: {lat: 41.152615, lng: -73.127059},
@@ -36,14 +29,13 @@ var model = {
 		 name: "Old Post Tavern",
 		 loc: {lat: 41.148220, lng: -73.255118},
 		 address: ["1418 Post Rd", "Fairfield", "CT"]
-		}]
-};
+		}];
 
 
 
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'), {
-	center: model.restaurants[0].loc,
+	center: model[0].loc,
 	zoom: 11
 	});
 
@@ -51,47 +43,240 @@ function initMap(){
 
 };
 
-function defaultMarkers(locations){
-		var i = 0;
-			//console.log(Object.keys(model));
-		Object.keys(model).forEach(function(key) {
-		var length = model[key].length;
-		for(i=0; i<length; i++){
-			//console.log(model[key][i].loc);
-			var marker = new google.maps.Marker({
-		    position: model[key][i].loc,
-		    map: map,
-		    title: 'Hello World!'
-			});
-			marker.setMap(map);
-		}
-	});
-};
-
-function LocationList(){
- 	var i = 0;
+function displayLocations(){
+ 	var i;
  	var locations = [];
-		Object.keys(model).forEach(function(key) {
-			var length = model[key].length;
-			for(i=0; i<length; i++){
-				eachLocation = model[key][i].name;
-				locations.push(eachLocation);
-				eachLocation
-
+	var length = model.length;
+		for(i=0; i<length; i++){
+			eachLocation = model[i].name;
+			locations.push(eachLocation);
 			}
-	});
+	console.log(locations);
 	return locations;
 };
 
+var marker;
+function defaultMarkers(locations){
+		console.log(locations);
+		var i;
+		var markers=[];
+		for(i=0; i<locations.length;i++){
+			for(j=0; j<model.length; j++){
+				if(locations[i] === model[j].name){
+					marker = new google.maps.Marker({
+					position: model[j].loc,
+					//animation: google.maps.Animation.DROP,
+					map: map,
+					title: model[i].name
+					});
+					marker.addListener('click', toggleBounce);
+					markers.push(marker);
+					marker.setMap(map);
+				}
+			}
+		}
+		function toggleBounce() {
+		  if (this.getAnimation() != null) {
+		  	console.log(this.title+"one");
+		  	console.log(this.getAnimation());
+			    this.setAnimation(null);
+		  } else {
+	  		  console.log(this.title+"two");
+	  		  this.setAnimation(google.maps.Animation.BOUNCE);
+			  }
+		}
 
+
+};
+
+
+
+
+// function markerAnimation(locations){
+// 	var length = locations.length;
+// 	var name = this;
+// 	google.maps.event.addDomListener(this, 'click', function() {
+
+
+// 	});
+
+
+// };
 
 
 function viewModel(){
 	var self = this;
-	self.displayMarkers = defaultMarkers();
-	self.locations = ko.observableArray(LocationList());
+	self.locations = ko.observableArray(displayLocations());
+	self.displayMarkers = defaultMarkers(self.locations());
+
+	//self.markerAnimation = markerAnimation();
 
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------
+// var map;
+// var model = {
+
+// 	restaurants: [
+// 		{
+// 		 name: "Paci Restaurant",
+// 		 loc: {lat: 41.143405, lng: -73.285961},
+// 		 address: ["Governor John Davis Lodge Turnpike", "Southport", "CT"]
+// 		},
+// 		{
+// 		 name: "Chips Restaurant",
+// 		 loc: {lat: 41.178543, lng: -73.240930},
+// 		 address: ["525 Tunxis Hill Cut Off", "Fairfield", "CT"]
+// 		}],
+
+// 	hotels: [
+// 		{
+// 		 name: "Holiday Inn",
+// 		 loc: {lat: 41.182853, lng: -73.182487},
+// 		 address: ["1070 Main St", "Bridgeport", "CT"]
+// 		},
+// 		{
+// 		 name: "Rodeway Inn",
+// 		 loc: {lat: 41.147831, lng: -73.129329},
+// 		 address: ["10 Washington Pkwy", "Stratford", "CT"]
+// 		}],
+
+// 	bars_and_pubs: [
+// 		{
+// 		 name: "Little Barn",
+// 		 loc: {lat: 41.152615, lng: -73.127059},
+// 		 address: ["1050 Post Rd E", "Westport", "CT"]
+// 		},
+// 		{
+// 		 name: "Old Post Tavern",
+// 		 loc: {lat: 41.148220, lng: -73.255118},
+// 		 address: ["1418 Post Rd", "Fairfield", "CT"]
+// 		}]
+// };
+
+
+
+// function initMap(){
+// 	map = new google.maps.Map(document.getElementById('map'), {
+// 	center: model.restaurants[0].loc,
+// 	zoom: 11
+// 	});
+
+// 	ko.applyBindings(new viewModel());
+
+// };
+
+
+
+// function defaultMarkers(locations){
+// 		var i = 0;
+// 		var markers=[];
+// 		Object.keys(model).forEach(function(key) {
+// 			var length = model[key].length;
+// 			for(i=0; i<length; i++){
+// 				//console.log(model[key][i].loc);
+// 				var marker = new google.maps.Marker({
+// 				    position: model[key][i].loc,
+// 				    //animation: google.maps.Animation.DROP,
+// 				    map: map,
+// 				    title: model[key][i].name
+// 				});
+
+// 				marker.addListener('click', toggleBounce);
+// 				markers.push(marker);
+// 				}
+
+// 				marker.setMap(map);
+
+// 				//console.log(marker);
+// 				function toggleBounce() {
+// 				  if (this.getAnimation() !== undefined) {
+// 				  	  this.setAnimation(undefined);
+// 				  } else {
+// 				  		 this.setAnimation(google.maps.Animation.BOUNCE);
+// 				  }
+// 				}
+
+
+// 		});
+// 	return markers;
+// };
+
+// function displayLocations(){
+//  	var i = 0;
+//  	var locations = [];
+// 		Object.keys(model).forEach(function(key) {
+// 			var length = model[key].length;
+// 			for(i=0; i<length; i++){
+// 				eachLocation = model[key][i].name;
+// 				locations.push(eachLocation);
+// 			}
+// 	});
+// 	return locations;
+// };
+
+// function markerAnimation(markers, locations){
+// 	console.log(markers);
+// 	var length = locations.length;
+// 	var name = this;
+// 	google.maps.event.addDomListener(this, 'click', function() {
+
+
+// 	});
+
+
+// };
+
+
+// function viewModel(){
+// 	var self = this;
+// 	self.displayMarkers = defaultMarkers();
+// 	self.locations = ko.observableArray(displayLocations());
+// 	self.markerAnimation = markerAnimation(self.displayMarkers, self.locations);
+
+
+// };
 
 
