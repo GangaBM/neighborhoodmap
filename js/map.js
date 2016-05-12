e62c378022ba46b23f80c3ce27d772f617b19c72
ganga <m.gangabhavani@gmail.com>
2016-05-11 22:41:23 -0400

    filter function implemented

--- js/map.js
+++ js/map.js
@@ -6,32 +6,26 @@ var infowindow;
 var places = [{
 		 name: "Paci Restaurant",
 		 loc: {lat: 41.143405, lng: -73.285961},
-		 address: ["Governor John Davis Lodge Turnpike", "Southport", "CT"]
 		},
 		{
 		 name: "Chips Restaurant",
 		 loc: {lat: 41.178543, lng: -73.240930},
-		 address: ["525 Tunxis Hill Cut Off", "Fairfield", "CT"]
 		},
 		{
 		 name: "Holiday Inn",
-		 loc: {lat: 41.182853, lng: -73.182487},
-		 address: ["1070 Main St", "Bridgeport", "CT"]
+		 loc: {lat: 41.182853, lng: -73.182487}
 		},
 		{
 		 name: "Rodeway Inn",
 		 loc: {lat: 41.147831, lng: -73.129329},
-		 address: ["10 Washington Pkwy", "Stratford", "CT"]
 		},
 		{
 		 name: "Little Barn",
 		 loc: {lat: 41.152615, lng: -73.127059},
-		 address: ["1050 Post Rd E", "Westport", "CT"]
 		},
 		{
 		 name: "Old Post Tavern",
 		 loc: {lat: 41.148220, lng: -73.255118},
-		 address: ["1418 Post Rd", "Fairfield", "CT"]
 		}];
 
 
@@ -44,43 +38,53 @@ function initMap(){
 
 	ko.applyBindings(new viewModel());
 
+
 };
+
+
+// function loadError()
+
+// }
 
 //extract the names of all the places into an array
-function locations(){
- 	var i;
- 	var locations = [];
-	var length = places.length;
-		for(i=0; i<length; i++){
-			eachLocation = places[i].name;
-			locations.push(eachLocation);
-			}
+// function locations(){
+//  	var i;
+//  	var locations = [];
+// 	var length = places.length;
+// 		for(i=0; i<length; i++){
+// 			eachLocation = places[i].name;
+// 			locations.push(eachLocation);
+// 			}
 
 
-	console.log(locations);
-	return locations;
-};
+// 	console.log(locations);
+// 	return locations;
+// };
 
 
 //displaying markers when the map is loaded
-function defaultMarkers(locations){
-		console.log(locations);
+function defaultMarkers(places){
+		console.log(places);
 		var i;
-
-		for(i=0; i<locations.length;i++){
+		// var contentString = "Hello";
+		for(i=0; i<places.length;i++){
 			marker = new google.maps.Marker({
 			position: places[i].loc,
 			animation: google.maps.Animation.DROP,
 			map: map,
-			title: locations[i]
+			draggable: false,
+			title: places[i].name
 			});
 
 			places[i].marker = marker;
+			markers.push(marker);
 
+			infowindow = new google.maps.InfoWindow();
 			marker.addListener('click', (function(marker){
 				return function(){
+
 					toggleBounce(marker);
-					getFourSquare(marker.title);
+					attachContent(marker);
 				}
 			}(marker)));
 
@@ -99,38 +103,82 @@ function toggleBounce(marker){
 
 
 function listClick(){
-	//marker = this.marker;
 	google.maps.event.trigger(this.marker, 'click');
 };
+// var contentString
+function attachContent(marker){
+		//console.log(marker);
+		var CLIENT_ID = "UZ10AIPRE0JSMOEEF5UXBSWVDC3XHU3J41XVP3LRJTBKY15X";
+		var CLIENT_SECRET = "SHLSIMV4VEK3N3RRYHUJ0FYUQLHLBEO4C0FKASIPGHRTEPCZ";
+		var fourSquareURL = "https://api.foursquare.com/v2/venues/search?client_id="+
+		CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&ll=41.14,-73.24&query="+marker.title+"&v=20140806";
+	    $.getJSON(fourSquareURL, function(data){
+	    	var venue = data.response.venues[0];
+	    	var address= venue.location.formattedAddress;
+	    	var url = venue.url;
 
-function getFourSquare(place){
-	var CLIENT_ID = "UZ10AIPRE0JSMOEEF5UXBSWVDC3XHU3J41XVP3LRJTBKY15X";
-	var CLIENT_SECRET = "SHLSIMV4VEK3N3RRYHUJ0FYUQLHLBEO4C0FKASIPGHRTEPCZ";
-	var fourSquareURL = "https://api.foursquare.com/v2/venues/search?client_id="+
-	CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&ll=41.14,-73.24&query="+place+"&v=20140806&m=foursquare";
-    $.getJSON(fourSquareURL, function(data){
-    	console.log(data);
 
-    }).error(function(e){
-        alert('foursquare Articles Could Not Be Loaded');
-    });
+	    	var contentString = '<div id="iwbody">'+
+	    	'<a href="'+url+'">website</a><br/>'+address[0]+'<br/>'+address[1]+'<br/>'+address[2]+'<br/><h3 id="phoneno">'+venue.contact.formattedPhone+'</h3><br/></div>';
 
-}
+	    	//console.log(data);
+			infowindow.setContent(contentString);
+			infowindow.open(map, marker);
+	    	//return contentString;
+
+
+	    }).error(function(e){
+	        //alert('foursquare Articles Could Not Be Loaded');
+	        infowindow.setContent('<div style="color:black">Sorry!! Cannot load the address</div>');
+	        infowindow.open(map, marker);
+	    });
+	};
+
+	// marker.addListener("click", function(){
+	// 	infowindow.open(marker.get('map'), marker);
+	// });
+
+
 
 function viewModel(){
-	var self = this;
-	self.locations = ko.observableArray(locations());
+	var self= this;
+	self.markers = ko.observableArray(markers);
+	//self.locations = ko.observableArray(locations());
 	self.places = ko.observableArray(places);
-	self.displayMarkers = defaultMarkers(self.locations());
+
+	self.query =  ko.observable('');
+
+	self.places = ko.computed(function() {
+        var search = self.query().toLowerCase();
+
+        return ko.utils.arrayFilter(places, function(place) {
+            	return place.name.toLowerCase().indexOf(search) >= 0;
+        });
+     }, self);
+
+
+
+	self.displayMarkers = defaultMarkers(self.places());
+
 	self.listToMarkerAnimation = listClick(self.displayMarkers);
-	// self.getInfoData = getFourSquare();
-	// self.markerClick = function(marker) {
-	// 	google.maps.event.trigger(marker, 'click', marker);
-	// };
-	//self.markerAnimation = markerAnimation();
+
+	self.clearMarkers = function(map, markers){
+		for(var i = 0; i < markers.length; i++){
+			markers[i].setMap(null);
+		}
+	};
+	self.showMarkers = function(map, markers){
+		for(var i = 0; i < markers.length; i++){
+			markers[i].marker.setMap(map);
+		}
+	}
+
+	self.sub = self.query.subscribe(function(){
+		self.clearMarkers(map, markers);
+		self.showMarkers(map, self.places());
+	});
+
 };
-
-
 
 
 
